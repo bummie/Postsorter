0879d5e6a0b8c0dc12939f8772dd6d11e6c82312
Seebzei <seebzei@gmail.com>
2016-10-31 13:04:33 +0100

    First upload

---
+++ Assets/Scripts/MoveHandler.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveHandler : MonoBehaviour {
+
+    private bool mouseHitPost = false, lockRotation = false;
+    private GameObject postObj;
+    private Vector3 lastObjPos;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 500.0f))
+            {
+                if (hit.transform.tag.Equals("post"))
+                {
+                    postObj = hit.transform.gameObject;
+                    ignoreAllPost(true);
+
+                    mouseHitPost = true;
+                    lockRotation = true;
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            if (mouseHitPost)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 500.0f))
+                {
+                    Debug.Log(ray.GetPoint(100f));
+                    if (hit.transform.tag.Equals("post_ray_collider"))
+                    {
+                        Vector3 nyPos = new Vector3(hit.point.x, hit.point.y + 3f, hit.point.z);
+                        postObj.transform.position = nyPos;
+                        if (lockRotation)
+                            postObj.transform.eulerAngles = Vector3.zero;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (postObj == null)
+                mouseHitPost = false;
+
+            if (mouseHitPost)
+            {
+                ignoreAllPost(false);
+                mouseHitPost = false;
+                lockRotation = false;
+            }
+        }
+    }
+
+    public void ignoreAllPost(bool ignore)
+    {
+        GameObject[] postObjekter = GameObject.FindGameObjectsWithTag("post");
+        foreach (GameObject g in postObjekter)
+        {
+            if (ignore)
+                g.layer = 2;
+            else
+                g.layer = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/PostInfo.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PostInfo : MonoBehaviour {
+
+    public float length = 1f, width = 1f, height = 1f;
+    public int post_type = 0; // 0 = Package, 1 = Small, 2 = Large;
+    public float paidPorto = 0f;
+    public float weight = 0f;
+    public bool stamped = false;
+
+	void Start ()
+    {
+	    
+	}
+	
+}
