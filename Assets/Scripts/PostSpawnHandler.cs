5f9d1a108bb527c171716f5c0b35f7e1e2ee6ada
Seebzei <seebzei@gmail.com>
2016-11-04 09:05:52 +0100

    - Litt mer på postSpawnInfo

--- Assets/Scripts/PostSpawnHandler.cs
+++ Assets/Scripts/PostSpawnHandler.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class PostSpawnHandler : MonoBehaviour
 {
-    Vector3 postSpawnPoint, postSpawnPointQueue;
+    private Vector3 postSpawnPoint, postSpawnPointQueue;
+    private GameObject postObjekter;
 
     void Start()
     {
+        postObjekter = GameObject.FindGameObjectWithTag("post_objekter");
         postSpawnPoint = GameObject.FindGameObjectWithTag("post_spawnpoint").transform.position;
         postSpawnPointQueue = GameObject.FindGameObjectWithTag("post_spawnpoint_queue").transform.position;
         createLetterSmall();
@@ -14,18 +16,55 @@ public class PostSpawnHandler : MonoBehaviour
 
     private GameObject createPackage()
     {
-        return null;
+        if (postObjekter != null)
+        {
+            GameObject package = Instantiate(Resources.Load("Objects/Post/pakke") as GameObject);
+            package.transform.parent = postObjekter.transform;
+            package.transform.position = postSpawnPoint;
+            package.GetComponent<PostInfo>().size = new Vector3(21f, 0.2f, 29.7f);
+            package.GetComponent<PostInfo>().weight = 21f;
+            package.GetComponent<PostInfo>().paidPorto = 11f;
+            package.GetComponent<PostInfo>().stamped = true;
+
+            return package;
+        }
+        else
+            return null;
     }
 
     private GameObject createLetterLarge()
     {
-        return null;
+        if (postObjekter != null)
+        {
+            GameObject largeLetter = Instantiate(Resources.Load("Objects/Post/brev_stort") as GameObject);
+            largeLetter.transform.parent = postObjekter.transform;
+            largeLetter.transform.position = postSpawnPoint;
+            largeLetter.GetComponent<PostInfo>().size = new Vector3(21f, 0.2f, 29.7f);
+            largeLetter.GetComponent<PostInfo>().weight = 48f;
+            largeLetter.GetComponent<PostInfo>().paidPorto = 17f;
+            largeLetter.GetComponent<PostInfo>().stamped = true;
+
+            return largeLetter;
+        }
+        else
+            return null;
     }
 
     private GameObject createLetterSmall()
     {
-        GameObject smallLetter = Instantiate(Resources.Load("Objects/Post/brev_lite") as GameObject);
-        smallLetter.transform.position = postSpawnPoint;
-        return smallLetter;
+        if (postObjekter != null)
+        {
+            GameObject smallLetter = Instantiate(Resources.Load("Objects/Post/brev_lite") as GameObject);
+            smallLetter.transform.parent = postObjekter.transform;
+            smallLetter.transform.position = postSpawnPoint;
+            smallLetter.GetComponent<PostInfo>().size = new Vector3(21f, 0.2f, 14.8f);
+            smallLetter.GetComponent<PostInfo>().weight = 21f;
+            smallLetter.GetComponent<PostInfo>().paidPorto = 11f;
+            smallLetter.GetComponent<PostInfo>().stamped = true;
+
+            return smallLetter;
+        }
+        else
+            return null;
     }
 }
