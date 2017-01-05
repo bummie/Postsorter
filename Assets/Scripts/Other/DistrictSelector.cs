ec7d3b67e884b24ce8ed8cf6e97ab8f3634ab81a
Seebzei <seebzei@gmail.com>
2017-01-05 20:15:00 +0100

    La til trigger på distriktene og begynte på HUD MAPS_SCENEN

---
+++ Assets/Scripts/Other/DistrictSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DistrictSelector : MonoBehaviour {
+
+    private int selectedDistrict = -1;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 600.0f))
+            {
+                if (hit.transform.tag.Equals("District"))
+                {
+                    string[] districtName = hit.transform.name.Split('_');
+                    Debug.Log("Selected district: " + districtName[1]);
+                    selectedDistrict = int.Parse(districtName[1]);
+                }
+            }
+        }
+    }
+}
